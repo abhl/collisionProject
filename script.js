bc9e0b41bce6b1f4e11e40a54823adb0c169ae16
abhishekASC4 <abhishekramjit@ymail.com>
2019-07-17 23:54:33 -0400

    please fix image

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ class Interactable{
     constructor(imagePath, frameDelay, frameCount, posX, posY, jumpChance,movementSpeed,height,width,health){
         this.gif = [];
         for(let i = 0; i < frameCount; i++){
-            this.gif[i] = loadImage(`assets/${imagePath}/frame_${i.pad(frameCount.toString().length)}_delay-0${frameDelay.toString()}s.gif`);
+            this.gif[i] = loadImage(`assets/${imagePath}/frame_${i.pad(frameCount.toString().length)}_delay-0${frameDelay.toString()}s.gif`,width,height);
             this.gif[i].resize(this.width,this.height);
         }
         this.x = posX;
